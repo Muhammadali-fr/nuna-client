5bebb412c39ca3075c9fc1837ffc2ae3f28d05b7
Muhammadali-fr <boburjonr934@gmail.com>
2025-05-14 22:05:14 +0500

    login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,10 +1,93 @@
-import LoginForm from "@/app/components/LoginForm";
+"use client"
+
+// components 
+import Loader from "@/app/components/Loader";
+
+// axios 
+import axios from "axios";
+
+// lucide 
+import { Mail } from "lucide-react";
+
+// next and react 
+import Link from "next/link";
+import { useState } from "react";
 
 const page = () => {
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    email: ""
+  });
+
+  const handleSubmit = async () => {
+    if (formData.email.length === 0) {
+      return alert('please check email')
+    }
+
+    try {
+      setLoading(true);
+      let res = await axios.post("https://nuna-core-server.onrender.com/auth/login", formData);
+
+      console.log(res);
+
+      // this is alert 
+      alert(res.data.message)
+    } catch (error) {
+      console.log(error);
+    } finally { setLoading(false) };
+  }
+
   return (
-    <>
-      <LoginForm />
-    </>
+    <div>
+      <h1 className="text-4xl font-bold mb-2">Log in without password</h1>
+      <p className="text-base text-gray-500 mb-8">
+        We will send a magic link to your email, just click it.
+      </p>
+
+      <form method="POST" className="w-1/2 mb-4">
+        <label htmlFor="email" className="block">
+          <span className="mb-2 text-gray-500 block">Your email</span>
+          <div className="flex items-center bg-gray-800 rounded-md px-4 space-x-4">
+            <Mail />
+            <input
+              id="email"
+              type="email"
+              value={formData.email}
+              onChange={e => setFormData({ ...formData, email: e.target.value })}
+              placeholder="mail@example.com"
+              required
+              className="h-12 w-full bg-transparent outline-none text-white"
+            />
+          </div>
+        </label>
+      </form>
+
+      <div className="flex flex-col items-start gap-2 mb-2">
+        <div className="flex items-start gap-2">
+          <label className="flex items-center">
+            <input type="checkbox" className="input" />
+            <span className="custom-checkbox"></span>
+          </label>
+          <p className="text-gray-500 flex items-center gap-1">
+            Allow all terms and cool stuff
+            <Link href="/terms" className="text-blue-600 underline">
+              in Nuna
+            </Link>
+          </p>
+        </div>
+        <Link href="/terms" className="text-blue-600 ">
+          Do you already have an account?
+        </Link>
+      </div>
+
+      <button
+        type="submit"
+        onClick={handleSubmit}
+        className="w-[200px] h-[40px] rounded bg-[#0C8CE9] cursor-pointer hover:opacity-80 flex items-center justify-center"
+      >
+        {loading ? <Loader /> : <span>Receive Link</span>}
+      </button>
+    </div>
   );
 };
 
---
+++ app/components/Loader.tsx
@@ -0,0 +1,10 @@
+// Loader.tsx
+import { BeatLoader } from 'react-spinners';
+
+const Loader = () => {
+    return (
+         <BeatLoader color="#ffffff" size={7}/>
+    );
+};
+
+export default Loader;
