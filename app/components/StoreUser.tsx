5bef3d2fd1613255e72b19d461614ab247514e68
Muhammadali-fr <boburjonr934@gmail.com>
2025-05-18 21:31:08 +0500

    nuna store image loader

--- app/components/StoreUser.tsx
+++ app/components/StoreUser.tsx
@@ -1,33 +1,51 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import authService from '../api/services/authService'
 import { setUser } from '@/lib/store/feature/userSlice'
 import { useDispatch } from 'react-redux'
 
+// assets 
+import Logo from "../icons/logo.svg";
+import Image from 'next/image'
+
 export default function StoreUser() {
     const router = useRouter()
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const storeuserinfo = async () => {
             const token = localStorage.getItem('token');
             if (!token) {
-                return router.push('/auth/login');
+                router.push('/auth/login');
+                return
             }
             try {
+                console.log("user malumotlari olinishni boshladi");
+
                 let res = await authService.getProfile();
                 dispatch(setUser(res));
             } catch (error) {
                 console.log(error, "this error while storing information");
                 return router.push('/auth/login');
-
-            }
+            } finally { setLoading(false) };
         }
         storeuserinfo();
 
-    }, [router])
+    }, [router, dispatch])
 
-    return null // no UI
+    if (loading) {
+        return (
+            <div className='fixed top-0 bottom-0 left-0 right-0 flex items-center justify-between flex-col bg-black text-white z-100 py-10'>
+                <span></span>
+                <Image src={Logo} width={120} height={120} alt="nuna logo" />
+                <p>Nuna</p>
+            </div>
+        )
+
+    }
+
+    return null
 }
